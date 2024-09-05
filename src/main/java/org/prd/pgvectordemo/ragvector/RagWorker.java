42153e339dcca8c15d4597e2f0ea9fa998bc1eab
Piero Alessandro Rodriguez Diaz <u21231696@utp.edu.pe>
2024-09-05 10:48:57 -0500

    Prueba pdf reader e instancias de pgvector

--- src/main/java/org/prd/pgvectordemo/ragvector/RagWorker.java
+++ src/main/java/org/prd/pgvectordemo/ragvector/RagWorker.java
@@ -4,10 +4,15 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.postgresql.util.PGobject;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.document.DocumentReader;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.PgVectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.simple.JdbcClient;
@@ -20,9 +25,12 @@ import java.util.List;
 public class RagWorker {
 
     @Autowired
+    @Qualifier("pgVectorStoreToRag")
     private PgVectorStore vectorStore;
+
     @Value("classpath:/static/PrdImportsa.pdf")
     private Resource pdfResource;
+
     @Autowired
     private JdbcClient jdbcClient;
 
@@ -30,7 +38,7 @@ public class RagWorker {
 
     @PostConstruct
     public void init() {
-        String sql = "SELECT count(*) FROM vector_store WHERE metadata->>'source' = :source";
+        String sql = "SELECT count(*) FROM vector_store WHERE metadata->>'file_name' = :source";
 
         Integer count = jdbcClient.sql(sql).
                 param("source",pdfResource.getFilename()).
@@ -41,13 +49,31 @@ public class RagWorker {
             return;
         }
 
-        TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(pdfResource);
+        /*TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(pdfResource);
         List<Document> documents = tikaDocumentReader.get();
-        TokenTextSplitter tokenTextSplitter = new TokenTextSplitter();
-        vectorStore.add(tokenTextSplitter.apply(documents));
+        var tokenTextSplitter = new TokenTextSplitter();
+        vectorStore.add(tokenTextSplitter.apply(documents));*/
+
+        DocumentReader reader =new PagePdfDocumentReader(
+                pdfResource,
+                PdfDocumentReaderConfig.builder()
+                        .withPageExtractedTextFormatter(ExtractedTextFormatter.builder()
+                                .withNumberOfBottomTextLinesToDelete(3)//eliminar las 3 ultimas lineas de texto de cada pagina  (footer)
+                                .withNumberOfTopPagesToSkipBeforeDelete(1) //saltar la primera pagina
+                                .build())
+                        .withPagesPerDocument(1)//cada pagina es un documento
+                        .build());
+        var textSplitter = new TokenTextSplitter();
+        //agregar metadata a los documentos
+        List<Document> documents = reader.get().stream().peek(document -> {
+            document.getMetadata().put("characters", document.getContent().length());
+        }).toList();
+        this.vectorStore.accept(textSplitter.apply(documents));
+        log.info("Documents metadata: {}", documents.stream().map(Document::getMetadata).toList());
         log.info("Vector pdf data created for {}", pdfResource.getFilename());
+
     }
-    /*
+    /*olla
      * Guarda el documento en el content con las configuraciones predeterminadas.
      * Cambiar los chunks por tokens para guardar en varias filas y tener una respuesta rapida.
      * -Probar con pdfReader (https://github.com/luisjavierjn/springai-sample-rag/blob/master/src/main/java/com/brianeno/springai/rag/dataloader/DataLoadingService.java)
